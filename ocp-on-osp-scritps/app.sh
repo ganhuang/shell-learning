926338e82892929f6ec06a75f7e1c1a2c154e485
Gan Huang <ghuang@redhat.com>
2017-06-06 20:52:24 +0800

    Update app.sh

--- ocp-on-osp-scritps/app.sh
+++ ocp-on-osp-scritps/app.sh
@@ -9,9 +9,10 @@ fi
 oc login $1
 oc new-project ghuang-test
 oc new-app cakephp-mysql-example
+oc logs -f bc/cakephp-mysql-example
 
 # Monitor the app route
-while :; do curl -s cakephp-mysql-example-install-test.0329-2rk.qe.rhcloud.com --output /dev/null -w "-status %{http_code} $(date)\n"; sleep 1; done
+while :; do curl -s cakephp-mysql-example-ghuang-test.cloudapps.ocp3.ghuang.com --output /dev/null -w "-status %{http_code} $(date)\n"; sleep 1; done
 
 # Update the stack
 openstack stack update --timeout 1200 -e ha-master-dedicated-flannel.yaml -t /usr/share/openshift-heat-templates/openshift.yaml ha-master-dedicated-flannel
