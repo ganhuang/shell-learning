ae3c59209899e7dfdc3c1b737616dc44ceb054c2
Gan Huang <ghuang@redhat.com>
2017-06-06 10:45:43 +0800

    add app.sh

---
+++ ocp-on-osp-scritps/app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# != 1 ];then
+  echo "Usage: $0 $LB/$Master"
+  exit 1
+fi
+
+# Login and create app
+oc login $1
+oc new-project ghuang-test
+oc new-app cakephp-mysql-example
+
+# Monitor the app route
+while :; do curl -s cakephp-mysql-example-install-test.0329-2rk.qe.rhcloud.com --output /dev/null -w "-status %{http_code} $(date)\n"; sleep 1; done
+
+# Update the stack
+openstack stack update --timeout 1200 -e ha-master-dedicated-flannel.yaml -t /usr/share/openshift-heat-templates/openshift.yaml ha-master-dedicated-flannel
+
