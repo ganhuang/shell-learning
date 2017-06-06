5bf30383198cc843fb6a5babb9330bf5f8c023de
Gan Huang <ghuang@redhat.com>
2017-06-06 10:36:10 +0800

    Update delete_stack.sh

--- ocp-on-osp-scritps/ocp34-on-osp10/ocp-templates/delete_stack.sh
+++ ocp-on-osp-scritps/ocp34-on-osp10/ocp-templates/delete_stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -x
 if [ $# -ne 1 ]; then 
     echo "Usage: $(basename $0) /path/to/template_file" 
     exit 1
@@ -14,14 +14,14 @@ if [ $? != 0 ];then
    exit 1
 fi
 
-if echo $(openstack stack show $stack_name) |grep -q DELETE_IN_PROGRESS;then
+if echo $(openstack stack show $stack_name) |grep -q -v DELETE_IN_PROGRESS;then
    openstack stack delete $stack_name
    sleep 10
 fi
 
-# https://github.com/redhat-openstack/openshift-on-openstack/blob/master/README_bugs.adoc
-for i in $(openstack resource list -n 2 $stack_name | grep PRO |grep deployment_bastion_node_cleanup | awk -F'|' '{printf $7}'); do
-  openstack stack resource signal $stack_name deployment_bastion_node_cleanup
+#https://github.com/redhat-openstack/openshift-on-openstack/blob/master/README_bugs.adoc
+for i in $(openstack stack resource list -n 2 $stack_name | grep PRO |grep deployment_bastion_node_cleanup | awk -F'|' '{printf $7}' | sed -e 's/ //g'); do
+  openstack stack resource signal $i deployment_bastion_node_cleanup
   echo $stack_name
   echo "========================================="
 done
