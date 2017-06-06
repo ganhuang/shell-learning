75aa7f222cedfd2dfe3bf04f161dc6a596597864
Gan Huang <ghuang@redhat.com>
2017-06-06 10:23:08 +0800

    Update delete_stack.sh

--- ocp-on-osp-scritps/ocp34-on-osp10/ocp-templates/delete_stack.sh
+++ ocp-on-osp-scritps/ocp34-on-osp10/ocp-templates/delete_stack.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+set -e
 if [ $# -ne 1 ]; then 
-    echo "Usage: $(basename $0) <stack_id/stack_name>" 
+    echo "Usage: $(basename $0) /path/to/template_file" 
     exit 1
 fi
 
-heat stack-show $1
+stack_name=$(echo "$1" | awk -F"/" '{print $NF}' | cut -d . -f 1)
+openstack stack show $stack_name
+
 if [ $? != 0 ];then
    echo "Not found the stack!"
    exit 1
 fi
 
-if [ -n $(heat stack-show $1 |grep DELETE_IN_PROGRESS) ];then
-   heat stack-delete $1
+if echo $(openstack stack show $stack_name) |grep -q DELETE_IN_PROGRESS;then
+   openstack stack delete $stack_name
    sleep 10
 fi
 
-for i in $(heat resource-list -n 2 $1|grep PRO |grep deployment_bastion_node_cleanup | awk -F'|' '{printf $7}'); do
-  heat resource-signal $i deployment_bastion_node_cleanup
-  echo $i
+# https://github.com/redhat-openstack/openshift-on-openstack/blob/master/README_bugs.adoc
+for i in $(openstack resource list -n 2 $stack_name | grep PRO |grep deployment_bastion_node_cleanup | awk -F'|' '{printf $7}'); do
+  openstack stack resource signal $stack_name deployment_bastion_node_cleanup
+  echo $stack_name
   echo "========================================="
 done
-
-
